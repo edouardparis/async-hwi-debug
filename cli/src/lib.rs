69d5943171db2d262e382e7f295106f7cada5fe7
edouardparis <m@edouard.paris>
2024-06-25 10:08:04 +0200

    Revert "remove every hw, except ledger"
    This reverts commit 0037fc9ff6a7082204c312d7efda297f306c69b3.

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -22,7 +22,81 @@ pub mod command {
     ) -> Result<Vec<Box<dyn HWI + Send>>, Box<dyn Error>> {
         let mut hws = Vec::new();
 
+        if let Ok(device) = SpecterSimulator::try_connect().await {
+            hws.push(device.into());
+        }
+
+        if let Ok(devices) = Specter::enumerate().await {
+            for device in devices {
+                hws.push(device.into());
+            }
+        }
+
+        match Jade::enumerate().await {
+            Err(e) => println!("{:?}", e),
+            Ok(devices) => {
+                for device in devices {
+                    let device = device.with_network(network);
+                    if let Ok(info) = device.get_info().await {
+                        if info.jade_state == jade::api::JadeState::Locked {
+                            if let Err(e) = device.auth().await {
+                                eprintln!("auth {:?}", e);
+                                continue;
+                            }
+                        }
+
+                        hws.push(device.into());
+                    }
+                }
+            }
+        }
+
+        if let Ok(device) = LedgerSimulator::try_connect().await {
+            hws.push(device.into());
+        }
+
         let api = Box::new(HidApi::new().unwrap());
+
+        for device_info in api.device_list() {
+            if async_hwi::bitbox::is_bitbox02(device_info) {
+                if let Ok(device) = device_info.open_device(&api) {
+                    if let Ok(device) =
+                        PairingBitbox02WithLocalCache::<runtime::TokioRuntime>::connect(
+                            device, None,
+                        )
+                        .await
+                    {
+                        if let Ok((device, _)) = device.wait_confirm().await {
+                            let mut bb02 = BitBox02::from(device).with_network(network);
+                            if let Some(ref policy) = wallet.as_ref().map(|w| w.policy).flatten() {
+                                bb02 = bb02.with_policy(policy)?;
+                            }
+                            hws.push(bb02.into());
+                        }
+                    }
+                }
+            }
+            if device_info.vendor_id() == coldcard::api::COINKITE_VID
+                && device_info.product_id() == coldcard::api::CKCC_PID
+            {
+                if let Some(sn) = device_info.serial_number() {
+                    if let Ok((cc, _)) = coldcard::api::Coldcard::open(&api, sn, None) {
+                        let mut hw = coldcard::Coldcard::from(cc);
+                        if let Some(ref wallet) = wallet {
+                            hw = hw.with_wallet_name(
+                                wallet
+                                    .name
+                                    .ok_or::<Box<dyn Error>>(
+                                        "coldcard requires a wallet name".into(),
+                                    )?
+                                    .to_string(),
+                            );
+                        }
+                        hws.push(hw.into())
+                    }
+                }
+            }
+        }
 
         for detected in Ledger::<TransportHID>::enumerate(&api) {
             if let Ok(mut device) = Ledger::<TransportHID>::connect(&api, detected) {
